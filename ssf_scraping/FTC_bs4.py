e1ddba584e91a9ebaf3e732713b80d4f92080a4a
jho7535 <127865770+jho7535@users.noreply.github.com>
2023-05-30 13:28:47 +0900

    Add files via upload

--- ssf_scraping/FTC_bs4.py
+++ ssf_scraping/FTC_bs4.py
@@ -2,29 +2,46 @@ from flask import Flask, render_template
 import requests
 from bs4 import BeautifulSoup
 
+url = "https://www.ssfshop.com/ranking?rankSect=SALE_RANK&ctgryFlterCd=CTGRY_MALE&preferAgeCd=ALL&brndShopId=&brandShopNo=&dspCtgryNo=&otltYn=&cnncCtgryNo=&lagSpcltyYn=&utag="
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36"
+}
+res = requests.get(url, headers=headers)
+res.raise_for_status()
+
+soup = BeautifulSoup(res.text, "lxml")
+
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    url = "https://www.ssfshop.com/ranking?rankSect=SALE_RANK&ctgryFlterCd=CTGRY_MALE&preferAgeCd=ALL&brndShopId=&brandShopNo=&dspCtgryNo=&otltYn=&cnncCtgryNo=&lagSpcltyYn=&utag="
-    headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36"
-    }
-    res = requests.get(url, headers=headers)
-    res.raise_for_status()
-
-    soup = BeautifulSoup(res.text, "lxml")
-
     items = soup.find_all("div", attrs={"class": "info"})
+    item_rankings = soup.find_all("div", attrs={"class": "rank"})
+    item_prices = soup.find_all("span", attrs={"class": "price"})
 
     product_list = []
 
-    for i in range(1, 20):
+    for i in range(1, 21):
         brand = items[i-1].find("span", attrs={"class": "brand"}).get_text()
         name = items[i-1].find("span", attrs={"class": "name"}).get_text()
-        product_list.append(f"{brand} - {name}")
+        rank = item_rankings[i-1].h5.get_text()
+        price = item_prices[i-1].get_text()
+        product_list.append(f"{rank} - {brand} - {name} - {price}")
 
-    return render_template('index.html', arr=product_list)
+    return render_template('index.html', pl=product_list)
+
+@app.route('/img')
+def img():
+    list_goods = soup.find("div", attrs={"class":"list_goods"})
+    lis = list_goods.ul.find_all("li")
+
+    item_images = []
+
+    for i in range(1, 21):
+        img = lis[i-1].a.img["src"]
+        item_images.append(f"{img}")
+
+    return render_template('index.html', img_urls=item_images)
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1')
