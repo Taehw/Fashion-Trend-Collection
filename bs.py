f24009196847742b71154a213bae7f7f6bc1a4bc
Taehyung Kim <thkim0517@gmail.com>
2023-05-30 15:53:54 +0900

    링크 추가

--- bs.py
+++ bs.py
@@ -15,6 +15,8 @@ ranking_items = soup.find_all('div', class_='brand')
 items_name = soup.find_all('div', class_='product ellipsis multiline')
 prices = soup.find_all('span', class_='discount_price')
 images = soup.find_all('img')
+thumbnail_list_top = soup.find("div", class_="thumbnail_list lst_top")
+thumbnail_list_bottom = soup.find("div", class_="thumbnail_list lst_bottom")
 
 # 랭킹 아이템에서 텍스트 추출하기
 for index, item in enumerate(ranking_items, start=1):
@@ -23,8 +25,22 @@ for index, item in enumerate(ranking_items, start=1):
     product_name = items_name[index-1].get_text(strip=True)
     price = prices[index-1].get_text(strip=True)
     image_url = images[index+5]['src']
+
     print(f"{rank}: {brand_name} - {product_name} - 가격 : {price}")
     print("Image URL:", image_url)
+
+    if thumbnail_list_top:
+        hrefs = thumbnail_list_top.find_all("a")
+        if len(hrefs) >= rank:
+            href = hrefs[rank-1]
+            link = 'https://www.wconcept.co.kr' + href['href']
+            print("링크:", link)
+    if rank >= 8 and thumbnail_list_bottom:
+        hrefs = thumbnail_list_bottom.find_all("a")
+        if len(hrefs) >= rank:
+            href = hrefs[rank-8]
+            link = 'https://www.wconcept.co.kr' + href['href']
+            print("링크:", link)
 
     if rank == 20:
         break
